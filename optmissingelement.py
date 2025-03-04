9311ecfb16189894bd673885f75739a50b439862
Nisheeka1604 <nisheekapudota@gmail.com>
2025-03-04 21:11:29 +0530

    new

--- optmissingelement.py
+++ optmissingelement.py
@@ -3,5 +3,5 @@ def find_missing_number(arr, n):
     actual_sum = sum(arr)
     return expected_sum - actual_sum
 
-print(find_missing_number([1, 2, 4, 5, 6], 6))  # Output: 3
-print(find_missing_number([1, 3], 3))           # Output: 2
+print(find_missing_number([1, 2, 4, 5, 6], 6))  
+print(find_missing_number([1, 3], 3))          
